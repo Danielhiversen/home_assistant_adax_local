a894174d4b746ed325937f2f80cac3926f507f92
Daniel Hjelseth Høyer <mail@dahoiv.net>
2020-11-04 16:22:14 +0100

    Improve config flow
    Signed-off-by: Daniel Hjelseth Høyer <mail@dahoiv.net>

--- custom_components/adax_local/config_flow.py
+++ custom_components/adax_local/config_flow.py
@@ -6,14 +6,17 @@ from homeassistant import config_entries, core, exceptions
 from homeassistant.const import CONF_TOKEN
 from homeassistant.util import slugify
 
+from .adax import AdaxConfig, HeaterNotAvailable, HeaterNotFound, InvalidWifiCred
 from .const import DEVICE_IP, DOMAIN
 
+WIFI_SSID = "wifi_ssid"
+WIFI_PSWD = "wifi_pswd"
 _LOGGER = logging.getLogger(__name__)
 
-DATA_SCHEMA = vol.Schema({vol.Required(DEVICE_IP): str, vol.Required(CONF_TOKEN): str})
+DATA_SCHEMA = vol.Schema({vol.Required(WIFI_SSID): str, vol.Required(WIFI_PSWD): str})
 
 
-async def validate_input(hass: core.HomeAssistant, device_ip):
+def validate_input(hass: core.HomeAssistant, device_ip):
     """Validate the user input allows us to connect."""
     for entry in hass.config_entries.async_entries(DOMAIN):
         if entry.data[DEVICE_IP] == device_ip:
@@ -32,16 +35,31 @@ class AdaxConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             try:
-                device_ip = user_input[DEVICE_IP].replace(" ", "")
-                token = user_input[CONF_TOKEN].replace(" ", "")
-                await validate_input(self.hass, device_ip)
-                unique_id = slugify(device_ip)
+                wifi_ssid = user_input[WIFI_SSID].replace(" ", "")
+                wifi_pswd = user_input[WIFI_PSWD].replace(" ", "")
+                configurator = AdaxConfig(wifi_ssid, wifi_pswd)
+                try:
+                    success = await configurator.configure_device()
+                except HeaterNotAvailable:
+                    return self.async_abort(reason="heater_not_available")
+                except HeaterNotFound:
+                    return self.async_abort(reason="heater_not_found")
+                except InvalidWifiCred:
+                    return self.async_abort(reason="invalid_wifi_cred")
+                if not success:
+                    raise CannotConnect
+
+                validate_input(self.hass, configurator.device_ip)
+                unique_id = slugify(configurator.device_ip)
                 await self.async_set_unique_id(unique_id)
                 self._abort_if_unique_id_configured()
 
                 return self.async_create_entry(
                     title=unique_id,
-                    data={DEVICE_IP: device_ip, CONF_TOKEN: token},
+                    data={
+                        DEVICE_IP: configurator.device_ip,
+                        CONF_TOKEN: configurator.access_token,
+                    },
                 )
 
             except AlreadyConfigured:
